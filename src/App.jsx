7cd154c1656594ebc1934e818e7627543fc52c1a
Swati-Malvi <swatimalvi11@gmail.com>
2023-11-27 00:07:48 +0530

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
 import LoginComponent from "./components/LoginComponent/LoginComponent";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginComponent />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <div>
-        <LoginComponent />
-      </div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
